56b40389fdd8dcd13886ce3090522a12b7b8ca5d
jinu <jinwoo794533@gmail.com>
2022-05-10 22:45:34 +0900

    KomoranService

---
+++ src/main/kotlin/com/ddns/dsqlbackendspringv1/global/komoran/service/KomoranService.kt
@@ -0,0 +1,6 @@
+package com.ddns.dsqlbackendspringv1.global.komoran.service
+
+interface KomoranService {
+
+    fun extractContent(content: String): List<String>
+}
---
+++ src/main/kotlin/com/ddns/dsqlbackendspringv1/global/komoran/service/KomoranSeviceImpl.kt
@@ -0,0 +1,31 @@
+package com.ddns.dsqlbackendspringv1.global.komoran.service
+
+import kr.co.shineware.nlp.komoran.core.Komoran
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+@Service
+class KomoranSeviceImpl(
+    private val komoran: Komoran
+): KomoranService {
+    private val log = LoggerFactory.getLogger(this.javaClass)
+
+    companion object{
+        const val DAEDOEK = "대덕"
+        const val SW = "소프트웨어"
+        const val MEISTER = "마이스터"
+    }
+
+
+    override fun extractContent(sentence: String): List<String> {
+        var s = sentence
+        if (sentence.contains(SW)||sentence.contains(MEISTER)) {
+            s = sentence.replace(SW, "").replace(MEISTER, "").replace(DAEDOEK, "")
+        }
+        val result = komoran.analyze(s)
+
+        val tags = result.nouns.stream().filter{ it.length >= 2 }.toList()
+        return tags
+
+    }
+}
