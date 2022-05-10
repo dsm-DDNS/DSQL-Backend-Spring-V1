81f9b14d89337368b5488bef81ce0f2b97c65ec1
jinu <jinwoo794533@gmail.com>
2022-05-11 08:52:23 +0900

    News Database

--- src/main/kotlin/com/ddns/dsqlbackendspringv1/domain/news/data/entity/News.kt
+++ src/main/kotlin/com/ddns/dsqlbackendspringv1/domain/news/data/entity/News.kt
@@ -1,6 +1,8 @@
-package com.ddns.dsqlbackendspringv1.domain.news.data
+package com.ddns.dsqlbackendspringv1.domain.news.data.entity
 
 import com.ddns.dsqlbackendspringv1.domain.auth.data.entity.user.User
+import com.ddns.dsqlbackendspringv1.domain.news.business.dto.FullNewsDto
+import com.ddns.dsqlbackendspringv1.domain.news.business.dto.ShortNewsDto
 import com.ddns.dsqlbackendspringv1.global.base.entity.BaseTimeEntity
 import java.time.LocalDate
 import javax.persistence.Entity
@@ -24,7 +26,7 @@ class News(
 
     var title: String = title
 
-    var shrotContent: String = shortContent
+    var shortContent: String = shortContent
 
     var content:String = content
 
@@ -34,7 +36,22 @@ class News(
 
     val createAt: LocalDate? = null
 
+    fun toShortNews(): ShortNewsDto {
+        return ShortNewsDto(
+            this.title,
+            this.shortContent,
+            this.writer.toMiniUserDto()
+        )
+    }
 
-
+    fun toFullNewsDto(): FullNewsDto {
+        return FullNewsDto(
+            this.title,
+            this.shortContent,
+            this.content,
+            this.writer.toFullUserDto(),
+            this.createAt!!
+            )
+    }
 
 }
---
+++ src/main/kotlin/com/ddns/dsqlbackendspringv1/domain/news/data/repository/NewsRepository.kt
@@ -0,0 +1,11 @@
+package com.ddns.dsqlbackendspringv1.domain.news.data.repository
+
+import com.ddns.dsqlbackendspringv1.domain.news.data.entity.News
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface NewsRepository: JpaRepository<News, Long> {
+    override fun findAll(pageable: Pageable): Page<News>
+    fun findFirstByOrderByCreateAtDesc(): News
+}
