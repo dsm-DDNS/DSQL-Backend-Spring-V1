909332730f94c198571f05f5dd816f0f8e31edd5
jinu <jinwoo794533@gmail.com>
2022-05-10 22:45:42 +0900

    ErroCode

---
+++ src/main/kotlin/com/ddns/dsqlbackendspringv1/global/error/ErrorCode.kt
@@ -0,0 +1,22 @@
+package com.ddns.dsqlbackendspringv1.global.error
+
+import org.springframework.http.HttpStatus
+
+enum class ErrorCode(
+    val message: String,
+    val status: HttpStatus
+) {
+
+    USERNAME_NOT_FOUND("Username Not Found", HttpStatus.NOT_FOUND),
+    INVALID_TOKEN("Invalid Token", HttpStatus.BAD_REQUEST),
+    EXPIRED_TOKEN("token Expired", HttpStatus.BAD_REQUEST),
+    HEADER_NOT_FOUND("Header Not Found", HttpStatus.NOT_FOUND),
+    S3_ERROR("S3 작업 과정 중 에러가 발생했습니다.", HttpStatus.BAD_GATEWAY),
+    EMAIL_ERROR("Email 전송 과정 중 에러가 발생했습니다.", HttpStatus.BAD_GATEWAY),
+    NOT_PERMIT_EMAIL_ERROR("Not Permit Email", HttpStatus.NOT_FOUND),
+    CHECK_EMAIL_CODE_ERROR("Error Occurred while Checking Email Code", HttpStatus.NOT_FOUND),
+    USER_NOT_FOUND("User Not Found", HttpStatus.NOT_FOUND),
+    INCORRECT_PASSWORD("Incorrect Password", HttpStatus.BAD_REQUEST),
+
+
+}
