7f7702f08327d1d4b91efb137659063d85187ab3
jinu <jinwoo794533@gmail.com>
2022-05-10 22:43:57 +0900

    Security&RelationConfig

---
+++ src/main/kotlin/com/ddns/dsqlbackendspringv1/global/security/config/FilterConfiguration.kt
@@ -0,0 +1,28 @@
+package com.ddns.dsqlbackendspringv1.global.security.config
+
+import com.ddns.dsqlbackendspringv1.global.error.filter.ExceptionFilter
+import com.ddns.dsqlbackendspringv1.global.security.jwt.JwtFilter
+import com.ddns.dsqlbackendspringv1.global.security.jwt.TokenProvider
+import com.ddns.dsqlbackendspringv1.global.security.jwt.auth.CustomAuthDetailsService
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.DefaultSecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+
+@Configuration
+class FilterConfiguration(
+    private val tokenProvider: TokenProvider,
+    private val customAuthDetailsService: CustomAuthDetailsService,
+    private val objectMapper: ObjectMapper
+) : SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>() {
+
+    override fun configure(builder: HttpSecurity) {
+        val jwtFilter = JwtFilter(tokenProvider, customAuthDetailsService)
+        val exceptionFilter = ExceptionFilter(objectMapper)
+        builder.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
+        builder.addFilterBefore(exceptionFilter, jwtFilter::class.java)
+    }
+}
---
+++ src/main/kotlin/com/ddns/dsqlbackendspringv1/global/security/config/SecurityConfiguration.kt
@@ -0,0 +1,59 @@
+package com.ddns.dsqlbackendspringv1.global.security.config
+
+import com.ddns.dsqlbackendspringv1.global.security.jwt.JwtFilter
+import com.ddns.dsqlbackendspringv1.global.security.jwt.TokenProvider
+import com.ddns.dsqlbackendspringv1.global.security.jwt.auth.CustomAuthDetailsService
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCrypt
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsUtils
+
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfiguration(
+    private val tokenProvider: TokenProvider,
+    private val customAuthDetailsService: CustomAuthDetailsService,
+    private val objectMapper: ObjectMapper
+): WebSecurityConfigurerAdapter() {
+
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    @Throws(Exception::class)
+    override fun configure(httpSecurity: HttpSecurity) {
+        httpSecurity
+            .csrf().disable()
+            .formLogin().disable()
+            .cors()
+
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+            .and()
+            .authorizeRequests()
+            .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+            .antMatchers(
+                "/api/**").permitAll()
+            .antMatchers("/api/auth/code").hasRole("ADMIN")
+            .anyRequest().permitAll()
+            .and()
+            .apply(FilterConfiguration(tokenProvider, customAuthDetailsService, objectMapper))
+    }
+
+
+
+
+}
