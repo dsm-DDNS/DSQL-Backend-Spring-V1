101f31c289222eeb21724e05e9680f3fd66a7d33
jinu <jinwoo794533@gmail.com>
2022-05-11 08:53:25 +0900

    add Auth Dto

---
+++ src/main/kotlin/com/ddns/dsqlbackendspringv1/domain/auth/business/dto/FullUserDto.kt
@@ -0,0 +1,12 @@
+package com.ddns.dsqlbackendspringv1.domain.auth.business.dto
+
+import com.ddns.dsqlbackendspringv1.domain.auth.data.entity.type.Role
+
+data class FullUserDto(
+    val id: String,
+    val role: Role,
+    val name: String,
+    val email: String,
+    val pw: String,
+    val introduction: String
+)
---
+++ src/main/kotlin/com/ddns/dsqlbackendspringv1/domain/auth/business/dto/MiniUserDto.kt
@@ -0,0 +1,7 @@
+package com.ddns.dsqlbackendspringv1.domain.auth.business.dto
+
+data class MiniUserDto (
+    val userId: String,
+    val name: String,
+    val email: String
+)
