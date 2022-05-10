4d0c63f17f48fd44e67294c1fc8965ea0819d9ba
jinu <jinwoo794533@gmail.com>
2022-05-11 08:51:59 +0900

    Project Dto

---
+++ src/main/kotlin/com/ddns/dsqlbackendspringv1/domain/project/business/dto/FullProjectDto.kt
@@ -0,0 +1,15 @@
+package com.ddns.dsqlbackendspringv1.domain.project.business.dto
+
+import com.ddns.dsqlbackendspringv1.domain.project.data.entity.Developer
+import com.ddns.dsqlbackendspringv1.domain.project.data.entity.Image
+import java.time.LocalDate
+
+data class FullProjectDto(
+    val title: String,
+    val introduction: String,
+    val startDate: LocalDate,
+    val endDate: LocalDate,
+    val devList: List<Developer>,
+    val imgList: List<Image>,
+
+)
---
+++ src/main/kotlin/com/ddns/dsqlbackendspringv1/domain/project/business/dto/ShortProject.kt
@@ -0,0 +1,11 @@
+package com.ddns.dsqlbackendspringv1.domain.project.business.dto
+
+import com.ddns.dsqlbackendspringv1.domain.project.data.entity.Image
+import com.ddns.dsqlbackendspringv1.domain.project.data.entity.UrlInfo
+
+data class ShortProject (
+    val title: String,
+    val img: List<Image>,
+    val introduction: String,
+    val urlList: List<UrlInfo>
+)
