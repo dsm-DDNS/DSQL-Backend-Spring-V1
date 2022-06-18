1635947b70c14d26dab156d1a30aa8b7077e1988
jinu <jinwoo794533@gmail.com>
2022-06-18 12:26:57 +0900

    EmailService 수정

--- src/main/kotlin/com/ddns/dsqlbackendspringv1/domain/auth/business/service/EmailServiceImpl.kt
+++ src/main/kotlin/com/ddns/dsqlbackendspringv1/domain/auth/business/service/EmailServiceImpl.kt
@@ -4,34 +4,34 @@ import com.ddns.dsqlbackendspringv1.domain.auth.exception.NotPermitEmailExceptio
 import com.ddns.dsqlbackendspringv1.domain.auth.business.service.emailCheck.PermitEmailListProperty
 import com.ddns.dsqlbackendspringv1.domain.auth.data.entity.token.EmailCheckCode
 import com.ddns.dsqlbackendspringv1.domain.auth.data.repository.token.EmailCheckCodeRepository
-import com.ddns.dsqlbackendspringv1.global.util.code.RandomCodeGenerator
 import com.ddns.dsqlbackendspringv1.infra.alarm.mail.MailService
+import org.apache.commons.lang3.RandomStringUtils
 import org.springframework.stereotype.Service
 
 
 @Service
 class EmailServiceImpl(
     private val mailService: MailService,
-    private val random: RandomCodeGenerator,
     private val emailCheckCodeRepository: EmailCheckCodeRepository,
-    private val emailList: PermitEmailListProperty
 ): EmailService {
 
     companion object{
         const val CODE_LENGTH = 4
         const val AUTH_MAIL_TITLE = "[DSQL] 인증번호입니다."
     }
+    private val emailList: MutableList<String> = ArrayList<String>()
 
     override fun sendCodeToEmail(email: String) {
-        if (emailList.email.contains(email)) {
-            val random = random.geneCode(CODE_LENGTH)
-            val emailCheckCode = EmailCheckCode(email, random)
-            emailCheckCodeRepository.save(emailCheckCode)
-            val map = HashMap<String, String>()
-            map.put("code", random)
-            mailService.sendHtmlMail(email, AUTH_MAIL_TITLE, "mail.html", map)
-        } else throw NotPermitEmailException(email)
+//        if (emailList.email.contains(email)) {
+        val random = RandomStringUtils.random(CODE_LENGTH)
+        val emailCheckCode = EmailCheckCode(email, random)
+        emailCheckCodeRepository.save(emailCheckCode)
+        val map = HashMap<String, String>()
+        map.put("code", random)
+        mailService.sendHtmlMail(email, AUTH_MAIL_TITLE, "mail.html", map)
+//        } else throw NotPermitEmailException(email)
     }
+
 
 
 }
