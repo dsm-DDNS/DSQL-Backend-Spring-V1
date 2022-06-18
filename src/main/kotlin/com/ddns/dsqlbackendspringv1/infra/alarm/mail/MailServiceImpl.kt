393b7d757f1a4f1725bedb79d1ae955ba6a89f5f
jinu <jinwoo794533@gmail.com>
2022-06-18 12:27:27 +0900

    Edit MailService

--- src/main/kotlin/com/ddns/dsqlbackendspringv1/infra/alarm/mail/MailServiceImpl.kt
+++ src/main/kotlin/com/ddns/dsqlbackendspringv1/infra/alarm/mail/MailServiceImpl.kt
@@ -6,7 +6,10 @@ import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.stereotype.Service
 import org.thymeleaf.TemplateEngine
 import org.thymeleaf.context.Context
+import javax.mail.Authenticator
 import javax.mail.MessagingException
+import javax.mail.Session
+import javax.mail.Transport
 import javax.mail.internet.MimeMessage
 
 
@@ -18,21 +21,21 @@ class MailServiceImpl(
 
 
     override fun sendHtmlMail(to: String, title: String, templatePath: String, models: Map<String, Any>) {
-        val message: MimeMessage = jms.createMimeMessage();
-        var context = Context();
-        models.forEach(context::setVariable);
-        val content: String = templateEngine.process(templatePath, context);
+        val message: MimeMessage = jms.createMimeMessage()
+        var context = Context()
+        models.forEach(context::setVariable)
+        val content: String = templateEngine.process(templatePath, context)
 
         try {
-            var helper = MimeMessageHelper(message, true);
-            helper.setSubject(title);
-            helper.setTo(to);
-            helper.setText(content, true);
+            val helper = MimeMessageHelper(message, true, "UTF-8")
+            helper.setFrom("dsmddns2022@naver.com")
+            helper.setSubject(title)
+            helper.setTo(to)
+            helper.setText(content, true)
         } catch (e: MessagingException) {
-            throw MailSendingException("SMTP 를 통해 메일 발송중 오류가 발생하였습니다!");
+            throw MailSendingException("SMTP 를 통해 메일 발송중 오류가 발생하였습니다!")
         }
-
-        jms.send(message);
+        jms.send(message)
     }
 
 
